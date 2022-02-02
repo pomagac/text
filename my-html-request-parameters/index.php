57cbcdd99104a8703bfa1b3235e327c7d4b4a1f6
pomagac <98908134+pomagac@users.noreply.github.com>
2022-02-02 19:27:44 +0100

    Added my-html-request-parameters (text endpoint).

---
+++ my-html-request-parameters/index.php
@@ -0,0 +1,24 @@
+<?php
+	
+	echo "<br><br>GET: <br>";
+	print_r($_GET);
+
+	echo "<br><br>POST: <br>";
+	print_r($_POST);
+
+	echo "<br><br>PUT: <br>";
+	print_r($_PUT);
+
+	echo "<br><br>HEAD: <br>";
+	print_r($_HEAD);
+	
+	echo "<br><br>DELETE: <br>";
+	print_r($_DELETE);
+
+	echo "<br><br>PATCH: <br>";
+	print_r($_PATCH);
+
+	echo "<br><br>OPTIONS: <br>";
+	print_r($_OPTIONS);
+
+?>
